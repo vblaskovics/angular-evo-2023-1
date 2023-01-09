c1d05ad17c0b76e6ef18ab1285bbeddd197500b2
viktor.blaskovics <viktor.blaskovics@gmail.com>
2023-01-09 13:46:03 +0100

    Add eventListener to addBtn

---
+++ 01_counter_js/app.js
@@ -0,0 +1,9 @@
+let counterDOM = document.querySelector("#counter");
+let addBtnDOM = document.querySelector("#addBtn");
+
+
+addBtnDOM.addEventListener("click", function () {
+  console.log('addBtnDOM clicked');  
+  // counterDOM.textContent = Number(counterDOM.textContent) + 1;
+});
+
