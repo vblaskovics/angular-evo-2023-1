8445defd39e5ee570194cb05a7f7c32d7165da72
viktor.blaskovics <viktor.blaskovics@gmail.com>
2023-01-09 14:20:23 +0100

    Add red rule

--- 01_counter_js/app.js
+++ 01_counter_js/app.js
@@ -3,6 +3,13 @@ let addBtnDOM = document.querySelector("#addBtn");
 
 
 addBtnDOM.addEventListener("click", function () {
-  counterDOM.textContent = Number(counterDOM.textContent) + 1;
+  let counterValue = Number(counterDOM.textContent);
+  counterValue += 1; 
+  counterDOM.textContent = counterValue;
+
+  if (counterValue >= 10) {
+    counterDOM.classList.add("red");
+  }
+ 
 });
 
