477a097800ffe13679b3e06e18e238e3956c1419
viktor.blaskovics <viktor.blaskovics@gmail.com>
2023-01-11 14:14:00 +0100

    Add list component

--- 10_tree_service/src/app/components/list/list.component.ts
+++ 10_tree_service/src/app/components/list/list.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Tree } from 'src/app/models/tree';
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
-  styleUrls: ['./list.component.css']
+  styleUrls: ['./list.component.css'],
 })
 export class ListComponent implements OnInit {
+  @Input() tree?: Tree;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  get todoList(): string[] {
+    let todoArr: string[] = [];
+    if (this.tree) {
+      this.getLabelsRecursively(this.tree, todoArr);
+    }
+    return todoArr;
   }
 
+  getLabelsRecursively(tree: Tree, todoArr: string[]): void {
+    if (tree.label) {
+      todoArr.push(tree.label);
+    }
+    if (tree.subTrees) {
+      tree.subTrees.forEach((subTree) => {
+        this.getLabelsRecursively(subTree, todoArr);
+      });
+    }
+  }
 }
